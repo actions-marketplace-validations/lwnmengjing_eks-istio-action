94be1e0057e107e07a3f14cbb2b5efe876c2c5a5
linwenxiang <991154416@qq.com>
2021-09-29 17:13:18 +0800

    🚑 : 去除角色

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -xe
 sh -c "aws configure set region ${aws_region}"
 sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
 sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
-sh -c "aws eks update-kubeconfig --region ${aws_region} --name ${cluster_name} --role-arn ${aws_role}"
+sh -c "aws eks update-kubeconfig --region ${aws_region} --name ${cluster_name}"
 
 #istio
 sh -c "curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${istio_version} sh -"
